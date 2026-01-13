8295a63bf7418278df6b7f8a7e38cbe6e8cdfa60
mparra-s <Magin.pa@gmail.com>
2026-01-13 17:35:26 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,22 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mparra-s <mparra-s@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/15 18:13:04 by mparra-s          #+#    #+#             */
-/*   Updated: 2026/01/12 11:56:11 by mparra-s         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
-
-/*char    ft_letterf(unsigned int a, char letter)
-{
-    letter = ft_toupper(letter);
-    return  (letter);
-}*/
 
 char	*ft_strmapi(char const *str, char (*fun)(unsigned int, char))
 {
